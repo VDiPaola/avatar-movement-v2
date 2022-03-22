c9c8911b507465947f87c06e5bdc1998883bd5cd
Enzo <dipaolae98@gmail.com>
2022-03-22 00:43:16 +0000

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace avatar_movent_v2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var waveIn = new WaveInEvent()
+            {
+                DeviceNumber = 0,
+                WaveFormat = new WaveFormat(rate: 44100, bits: 16, channels: 1),
+                BufferMilliseconds = 50
+            };
+            waveIn.DataAvailable += OnDataAvailable;
+            waveIn.StartRecording();
+        }
+
+        private void OnDataAvailable(object sender, WaveInEventArgs args)
+        {
+            float maxValue = 32767/4;
+            float ratio = 0f;
+            int peakValue = 0;
+            int bytesPerSample = 2;
+            for (int index = 0; index < args.BytesRecorded; index += bytesPerSample)
+            {
+                int value = BitConverter.ToInt16(args.Buffer, index);
+                peakValue = Math.Max(peakValue, value);
+
+                ratio = peakValue / maxValue;
+            }
+
+            Dispatcher.Invoke(() =>
+            {
+                //update UI
+                var maxRotation = -50;
+                ImageTop.Angle = maxRotation * ratio;
+            });
+        }
+    }
+}
